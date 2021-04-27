e3339ea2ed4cabfc998a22e7d339de5268f48dce
xiaotianxt <1241811980@qq.com>
2021-04-27 10:03:44 +0800

    init method

--- geometry/geo_collection.cs
+++ geometry/geo_collection.cs
@@ -5,7 +5,17 @@ using System.Text;
 
 namespace DEETU.geometry
 {
-    class geo_collection
+    public class geo_collection
     {
+        // Properties
+        private List<geo_geometry> _item_list { get; }
+
+        // Methods
+        public geo_collection(List<geo_geometry> item_list)
+        {
+            _item_list = item_list;
+        }
+
+
     }
 }
