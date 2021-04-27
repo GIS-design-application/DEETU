8cbefa29e06bc0659a5ecb96aa793fc2a4ff8360
yinghualanyan <709645802@qq.com>
2021-04-27 11:46:13 +0800

    create polygon

---
+++ geometry/GeoLinearRing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DEETU.geometry
+{
+    class GeoLinearRing
+    {
+        public bool IsEmpty()
+        {
+            return true;
+        }
+    }
+}
