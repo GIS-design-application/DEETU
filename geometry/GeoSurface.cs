9c0d06cf5bcabd0b5f1196202efc5675b7c40fe1
yinghualanyan <709645802@qq.com>
2022-09-13 11:46:13 +0800

    create polygon

---
+++ geometry/GeoSurface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DEETU.geometry
+{
+    abstract class GeoSurface : GeoGeometry
+    {
+    }
+}
