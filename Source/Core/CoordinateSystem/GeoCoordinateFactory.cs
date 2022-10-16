252060aefe5c3f27efb4bc8c35d5c3d4b4ba2e80
维昱 张 <zwy990929@pku.edu.cn>
2022-10-16 21:55:59 +0800

    完成坐标系统GeoCoordinateReferenceSystem类和GeoCoordinateFactory类框架

---
+++ Source/Core/CoordinateSystem/GeoCoordinateFactory.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DEETU.Core;
+using DEETU.Geometry;
+
+namespace DEETU.Source.Core.CoordinateSystem
+{
+    /// <summary>
+    /// 投影类型枚举
+    /// 共支持4种坐标系统，2种地理坐标系，2种投影坐标系
+    /// </summary>
+    public enum CrsName
+    {
+        WGS84,
+        Beijing1954,
+        WebMercator,
+        Lambert2SP
+    }
+    public enum CrsType
+    {
+        None,
+        Geographic,
+        Projected
+    }
+    /// <summary>
+    /// 坐标系工厂类，提供工具函数
+    /// </summary>
+    public class GeoCoordinateFactory
+    {
+        //存储四种坐标系的默认参数
+        static Dictionary<string,string> _WGS84Param;
+        static Dictionary<string, string> _Beijing1954Param;
+        static Dictionary<string, string> _WebMercatorParam;
+        static Dictionary<string, string> _Lambert2SPParam;
+
+
+        public void LambertToGeographic(GeoPoints points)
+        {
+
+        }
+        public void GeographicToLambert(GeoPoints points)
+        {
+
+        }
+        public void WebMercatorToWGS84(GeoPoints points)
+        {
+
+        }
+        public void WGS84ToWebMercator(GeoPoints points)
+        {
+
+        }
+        public void WGS84ToBeijing1954(GeoPoints points)
+        {
+
+        }
+        public void Beijing1954ToWGS84(GeoPoints points)
+        {
+
+        }
+
+    }
+}
---
+++ Source/Core/CoordinateSystem/GeoCoordinateReferenceSystem.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DEETU.Core;
+
+
+namespace DEETU.Source.Core.CoordinateSystem
+{
+    /// <summary>
+    /// 用来记录图层或地图的坐标系统
+    /// </summary>
+    public class GeoCoordinateReferenceSystem
+    {
+
+        CrsName? _GeographicCrs;//可空枚举
+        CrsName? _ProjectedCrs;
+        Dictionary<string, string> _Parameters;
+        string _Unit;//单位
+
+        GeoCoordinateReferenceSystem()
+        {
+            _GeographicCrs = null;
+            _ProjectedCrs = null;
+            _Unit = null;
+            _Parameters = new Dictionary<string, string>();
+        }
+
+        #region 属性
+        public bool IsEmpty
+        {
+            get
+            {
+                if (_GeographicCrs == null) return true;
+                return false;
+            }
+        }
+        /// <summary>
+        /// 坐标系类型
+        /// </summary>
+        public CrsType Type
+        {
+            get {
+                if (_GeographicCrs == null) return CrsType.None;
+                else if (_ProjectedCrs == null) return CrsType.Geographic;
+                else return CrsType.Projected;
+            }
+        }
+        #endregion
+
+        #region 方法
+        public void SetParameters(string key, string value)
+        {
+            if(_Parameters.ContainsKey(key))
+            {
+                _Parameters[key] = value;
+            }
+            else
+            {
+                throw new Exception("parameter is not existing,please check crs name");
+            }
+        }
+        /// <summary>
+        /// 更改投影类型，但不更改数据。
+        /// 使用前提示用户确认
+        /// </summary>
+        /// <param name="newCrs"></param>
+        public void ChangeCrs(CrsName? newProjectedCrs=null,CrsName? newGeographicCrs=null)
+        {
+            if(newGeographicCrs == CrsName.WebMercator)
+            {
+                _GeographicCrs = CrsName.WGS84;
+                _ProjectedCrs = CrsName.WebMercator;
+            }
+            else if(newCrs==CrsName.Beijing1954||newCrs==CrsName.WGS84)
+            {
+
+            }
+        }
+        #endregion
+    }
+}
