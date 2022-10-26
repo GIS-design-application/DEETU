32fbd8db3d30bbf506b5cf2d69428aaf062ad97a
xiaotianxt <tianyp@pku.edu.cn>
2022-10-26 21:31:04 +0800

    添加选择模式框架, 添加注释

--- Source/Window/MainPage.cs
+++ Source/Window/MainPage.cs
@@ -299,6 +299,7 @@ namespace DEETU.Source.Window
             }
         }
 
+        #region 渲染部分代码(弃用)
         // 简单渲染
         private void btnSimpleRender_Click(object sender, EventArgs e)
         {
@@ -396,6 +397,7 @@ namespace DEETU.Source.Window
             sLayer.LabelRenderer = sLabelRenderer;
             geoMap.RedrawMap();
         }
+        #endregion
 
         // 移动多边形
         private void btnMovePolygon_Click(object sender, EventArgs e)
@@ -1257,6 +1259,11 @@ OnZoomOut_MouseUp(e);
                     break;
             }
         }
+
+        /// <summary>
+        /// 标记toolstripbutton已经被点击
+        /// </summary>
+        /// <param name="mapOpStyle"></param>
 
         private void CheckToolStrip(GeoMapOpStyleEnum mapOpStyle)
         {
