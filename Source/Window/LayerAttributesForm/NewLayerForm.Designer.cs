d364cf2abf787b1e7efe0e6534575fa58e43d6b3
xiaotianxt <tianyp@pku.edu.cn>
2022-11-03 00:44:19 +0800

    test

---
+++ Source/Window/LayerAttributesForm/NewLayerForm.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace DEETU.Source.Window.LayerAttributes
+{
+    partial class NewLayerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // NewLayerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 485);
+            this.Name = "NewLayerForm";
+            this.Style = Sunny.UI.UIStyle.Custom;
+            this.Text = "新建图层";
+            this.TitleColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(118)))), ((int)(((byte)(118)))));
+            this.TitleForeColor = System.Drawing.Color.Black;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Window/LayerAttributesForm/NewLayerForm.cs
@@ -0,0 +1,17 @@
+﻿using DEETU.Core;
+using DEETU.Map;
+using DEETU.Tool;
+using Sunny.UI;
+
+namespace DEETU.Source.Window.LayerAttributes
+{
+    public partial class NewLayerForm : UIMainFrame
+    {
+        public NewLayerForm()
+        {
+            InitializeComponent();
+            
+
+        }
+    }
+}
